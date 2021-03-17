3e4c970a984001c92e4177187807608df98c8e70
A struggling coder <56886686+RD-Joey@users.noreply.github.com>
2021-03-17 19:48:01 +0800

    added 'quit' request

--- src/main/java/com/pieceofpie/py/controller/EmotionController.java
+++ src/main/java/com/pieceofpie/py/controller/EmotionController.java
@@ -3,9 +3,9 @@ package com.pieceofpie.py.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 @RestController
 @RequestMapping("emotion")
@@ -16,17 +16,18 @@ public class EmotionController {
         Process proc;
         try {
             proc = Runtime.getRuntime().exec("C:\\Users\\asus\\Desktop\\pythonProject1\\test.bat");// 执行py文件
-            //用输入输出流来截取结果
-            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            String line = null;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
-            }
-            in.close();
-            proc.waitFor();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        }
+    }
+
+    @RequestMapping("quit")
+    public void quit(){
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("quit.txt"));
+            out.write("q");
+            out.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
